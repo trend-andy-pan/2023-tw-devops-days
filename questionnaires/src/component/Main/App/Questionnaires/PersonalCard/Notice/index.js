2e27b972ce4a0d2b66ad94382c2c2a68358cd1e6
Andy Pan <andy_pan@trendmicro.com>
2023-09-19 10:20:23 +0800

    style: change notice style

--- questionnaires/src/component/Main/App/Questionnaires/PersonalCard/Notice/index.js
+++ questionnaires/src/component/Main/App/Questionnaires/PersonalCard/Notice/index.js
@@ -3,11 +3,13 @@ import { AiOutlineNotification } from "react-icons/ai";
 
 export default function Notice() {
   return (
-    <div className="gap-1 flex flex-row items-top bg-transparent border-warning text-warning border-medium rounded-medium p-unit-4">
-      <p className="flex-none">貼心提醒</p>
-      <AiOutlineNotification className="flex-none w-6 h-6" />
-      <p className="flex-auto">
-        欲收到 report 及參加現場抽大獎 <strong>Nespresso 膠囊咖啡機</strong>{" "}
+    <div className="bg-transparent border-warning text-warning border-medium rounded-medium p-unit-4">
+      <p>
+        貼心提醒
+        <AiOutlineNotification className="inline-block px-1 w-6 h-6" />
+        欲收到 report 及參加現場抽大獎 <strong>
+          Nespresso 膠囊咖啡機
+        </strong>{" "}
         者，請幫忙留下正確個資，我們才有辦法確認獲獎喔！
       </p>
     </div>
